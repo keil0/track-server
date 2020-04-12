198ad30a2f41a1f25dcbea74da24ea9a12a2ad44
Pierre Cruz <contact@pierrecruz.com>
2020-04-12 18:04:54 +0200

    Start #176

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -9,10 +9,13 @@ router.post('/signUp', async (req, res) => {
   console.log(req.body);
   const { email, password } = req.body;
 
-  const user = new User({email, password});
-  await user.save();
-
-  res.send('You made a post request');
+  try {
+    const user = new User({email, password});
+    await user.save();
+    res.send('You made a post request');
+  } catch (err) {
+    res.status(422).send(err.message);
+  }
 });
 
 module.exports = router;
